976e8ea34f8a4865458f7edf7d358b9429d9e068
KavishkaSasindu <sasindu0215@gmail.com>
2024-04-23 01:12:49 +0530

    updated loading screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,20 @@ export class AppComponent {
 
   myData:any = []
 
+   loading:boolean =false;
+
   constructor(private http: HttpClient) {
   }
 
   loadData() {
+    this.loading=true;
     this.http.get("https://jsonplaceholder.typicode.com/comments")
       .subscribe(response=>{
         console.log(response);
         this.myData = response
+        setTimeout(()=>{
+          this.loading =false;
+        },5000)
       })
   }
 
