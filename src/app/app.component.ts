2f00d4e350f21965a52d1006ff8ba5564fb47e05
KavishkaSasindu <sasindu0215@gmail.com>
2024-04-22 19:41:27 +0530

    updated search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app1';
+
+  dataArray = [
+    'java',
+    'php',
+    'perl',
+    'python',
+    'c++',
+    'c#'
+  ]
+
+  searchValue = '';
+
+  filterList:any = [];
+
+  search(searchValue: any){
+    this.filterList = []
+    this.searchValue = searchValue;
+
+    for(const data of this.dataArray){
+      if(data.toLocaleLowerCase().includes(this.searchValue.toLocaleLowerCase())){
+        this.filterList.push(data);
+      }
+    }
+  }
+
+
 }
